79d420b780e77bcc3af9ffe728d9c529fce9ba77
abid <abidshahriar7@gmail.com>
2021-11-07 05:17:59 +0600

    added most of the functionality

--- src/App.tsx
+++ src/App.tsx
@@ -24,24 +24,62 @@ const data = [
 
 export default function App() {
   const [cards, setCards] = useState<{ id: number; img: string }[] | undefined>(undefined);
+  const [firstFlip, setFirstFlip] = useState<any>('');
+  const [secondFlip, setSecondFlip] = useState<any>('');
 
   function shuffleCards() {
     const newArrOfCards = [...data, ...data].map((item) => ({ ...item, id: Math.random() }));
 
     const shuffledCards = newArrOfCards.sort(() => Math.random() - 0.5);
 
+    const allCards = document.querySelectorAll('.card');
+
+    allCards.forEach((card: any) => {
+      card.classList.remove('done');
+    });
+
     setCards(shuffledCards);
+  }
+
+  function showHideAllCards(showOrHide: string) {
+    document.body.style.pointerEvents = showOrHide === 'show' ? 'none' : 'all';
+    const allCardsFront = document.querySelectorAll('.card-front');
+
+    allCardsFront.forEach((card: any) => {
+      card.style.transform = showOrHide === 'show' ? 'rotateY(0deg)' : 'rotateY(90deg)';
+    });
   }
 
   useEffect(() => {
     shuffleCards();
   }, []);
 
+  useEffect(() => {
+    showHideAllCards('show');
+
+    setTimeout(() => {
+      showHideAllCards('hide');
+    }, 5000);
+  }, [cards]);
+
   return (
-    <div className='grid grid-cols-4 gap-4 max-w-[800px]  mx-auto '>
-      {cards?.map((card, idx: number) => (
-        <Card key={card.id} img={`/svgs/${card.img}`} />
-      ))}
+    <div>
+      <div className='grid grid-cols-4 gap-4 max-w-[800px] mx-auto'>
+        {cards?.map((card, idx: number) => (
+          <Card
+            key={card.id}
+            img={`/svgs/${card.img}`}
+            setFirstFlip={setFirstFlip}
+            setSecondFlip={setSecondFlip}
+            firstFlip={firstFlip}
+            secondFlip={secondFlip}
+          />
+        ))}
+
+        <button className='text-yellow-50 text-2xl uppercase px-1 py-1 border-2 rounded-lg' onClick={shuffleCards}>
+          reset
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,62 @@
 interface CardProps {
   img: string;
+  setFirstFlip: Function;
+  setSecondFlip: Function;
+  firstFlip: any;
+  secondFlip: any;
 }
 
-export default function Card({ img }: CardProps) {
+export default function Card({ img, setFirstFlip, setSecondFlip, firstFlip, secondFlip }: CardProps) {
+  let previousElement: any = '';
+
   function reveal(e: any) {
+    if (e.target.parentElement.classList.contains('done')) return;
+
+    if (!firstFlip) {
+      setFirstFlip(e.target.previousElementSibling);
+      e.target.previousElementSibling.style.transform = 'rotateY(0)';
+      return;
+    }
+
+    if (firstFlip && firstFlip.src === e.target.previousElementSibling.src) {
+      firstFlip.parentElement.classList.add('done');
+      e.target.parentElement.classList.add('done');
+      e.target.previousElementSibling.style.transform = 'rotateY(0deg)';
+
+      setFirstFlip('');
+      return;
+    }
+
+    document.body.style.pointerEvents = 'none';
     e.target.previousElementSibling.style.transform = 'rotateY(0)';
+
+    setTimeout(() => {
+      firstFlip.style.transform = 'rotateY(90deg)';
+      e.target.previousElementSibling.style.transform = 'rotateY(90deg)';
+
+      setFirstFlip('');
+
+      document.body.style.pointerEvents = 'all';
+    }, 1500);
   }
 
   function hide(e: any) {
+    if (e.target.parentElement.classList.contains('done')) return;
+
     e.target.style.transform = 'rotateY(90deg)';
+    setFirstFlip(undefined);
   }
 
   return (
-    <div className='relative flex items-center justify-center'>
+    <div className='card relative flex items-center justify-center'>
       <img
         src={img}
         alt='memory'
-        className='rounded-lg shadow-lg absolute transition-all duration-500'
+        className='card-front rounded-lg shadow-lg absolute transition-all duration-500'
         style={{ transform: 'rotateY(90deg)' }}
         onClick={hide}
       />
-      <img src='/svgs/cover.svg' alt='memory' className='rounded-lg shadow-lg' onClick={reveal} />
+      <img src='/svgs/cover.svg' alt='memory' className='card-back rounded-lg shadow-lg' onClick={reveal} />
     </div>
   );
 }
